a6c3d4d9954d1212f0667ef10dc5be54dd7e489d
conqcuong <conqcuong@gmail.com>
2024-09-22 10:08:21 +0700

    test

--- AFT_BookStore/Controllers/AuthController.cs
+++ AFT_BookStore/Controllers/AuthController.cs
@@ -15,5 +15,7 @@ namespace AFT_BookStore.Controllers
         {
             return View();
         }
+
+        helllo
     }
 }
--- AFT_BookStore/obj/Debug/net6.0/AFT_BookStore.AssemblyInfo.cs
+++ AFT_BookStore/obj/Debug/net6.0/AFT_BookStore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AFT_BookStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64594791fa1b5d3932ecb40e3efd9b14770479c2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad1359b9e2830f05e5976934b2f641e7dc0cac44")]
 [assembly: System.Reflection.AssemblyProductAttribute("AFT_BookStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AFT_BookStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
